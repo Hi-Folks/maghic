52ce46ebd6d04f8dc973dde123e48ce8f0d843e1
Roberto B <roberto.butti@gmail.com>
2021-11-29 21:27:50 +0100

    test coverage 100%

--- tests/Unit/YamlObjectServiceTest.php
+++ tests/Unit/YamlObjectServiceTest.php
@@ -6,9 +6,15 @@ test('Get Service Image', function () {
 });
 test('GetSet Service Image', function () {
     $y = \App\Objects\YamlObject::load(__DIR__ . "/../data/003.yaml");
+    expect($y->getFirstJobName())->toBe("laravel-tests");
     expect($y->setCurrentJobName("laravel-tests")->getImage())->toBe("mysql:5.7");
     expect($y->setImage("mysql:latest"));
     expect($y->getImage())->toBe("mysql:latest");
+});
+test('GetFirst job name', function () {
+    $y = \App\Objects\YamlObject::make();
+    expect($y->getFirstJobName())->toBe("build");
+
 });
 
 test('Add Mysql service', function () {
